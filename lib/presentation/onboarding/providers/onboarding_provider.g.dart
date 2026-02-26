3d3d65b3f7b3ce17b203b2f592dacf6f6223255c
SJG <shinjg97@gmail.com>
2026-02-26 20:06:12 +0900

    Launcher icon created<+splash> <Unversioned Files>

---
+++ lib/presentation/onboarding/providers/onboarding_provider.g.dart
@@ -0,0 +1,64 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'onboarding_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint, type=warning
+
+@ProviderFor(OnboardingNotifier)
+const onboardingProvider = OnboardingNotifierProvider._();
+
+final class OnboardingNotifierProvider
+    extends $NotifierProvider<OnboardingNotifier, bool> {
+  const OnboardingNotifierProvider._()
+    : super(
+        from: null,
+        argument: null,
+        retry: null,
+        name: r'onboardingProvider',
+        isAutoDispose: true,
+        dependencies: null,
+        $allTransitiveDependencies: null,
+      );
+
+  @override
+  String debugGetCreateSourceHash() => _$onboardingNotifierHash();
+
+  @$internal
+  @override
+  OnboardingNotifier create() => OnboardingNotifier();
+
+  /// {@macro riverpod.override_with_value}
+  Override overrideWithValue(bool value) {
+    return $ProviderOverride(
+      origin: this,
+      providerOverride: $SyncValueProvider<bool>(value),
+    );
+  }
+}
+
+String _$onboardingNotifierHash() =>
+    r'cc143629b9c751b46a0dfa0707452dc257ada9ca';
+
+abstract class _$OnboardingNotifier extends $Notifier<bool> {
+  bool build();
+  @$mustCallSuper
+  @override
+  void runBuild() {
+    final created = build();
+    final ref = this.ref as $Ref<bool, bool>;
+    final element =
+        ref.element
+            as $ClassProviderElement<
+              AnyNotifier<bool, bool>,
+              bool,
+              Object?,
+              Object?
+            >;
+    element.handleValue(ref, created);
+  }
+}
