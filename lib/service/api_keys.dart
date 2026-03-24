7c4b14d154771a142238345644111165844bec40
SJG <shinjg97@gmail.com>
2026-03-24 22:25:35 +0900

    (reverted) content_card.dart

--- lib/service/api_keys.dart
+++
@@ -1,8 +0,0 @@
-class WeatherAPI{
-  static String BASEURL = "https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0";
-  static String ENDPOINT = "getUltraSrtNcst";
-  static String APIKEY = "eea8f45523d090dd4005a45de6f38effceaf2804f9be7869dff8fd76fcd8bf98";
-}
-class GeminiAPI{
-  static String GEMINI_APIKEY = "AIzaSyBeX9r06gIOM-8Ydq8dTH5jqrtgwnKZiRI";
-}
